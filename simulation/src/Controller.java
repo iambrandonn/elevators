efb27875f16dd1793623f17fb53e410516f6c243
Brandon Nicholls <brandon.nicholls@gmail.com>
2014-12-20 14:13:03 -0700

    Starting movement logic

--- simulation/src/Controller.java
+++ simulation/src/Controller.java
@@ -1,14 +1,19 @@
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class Controller {
     private int numFloors;
 
+    // Keeps track of the status of elevators
     private class ElevatorReference {
         private int currentFloor;
         private boolean doorsOpen;
+        private Direction direction;
+        private final int id;
 
-        public ElevatorReference() {
-
+        public ElevatorReference(int id) {
+            this.id = id;
+            this.direction = Direction.NotMoving;
         }
 
         public boolean isDoorsOpen() {
@@ -26,6 +31,10 @@ public class Controller {
         public void setCurrentFloor(int currentFloor) {
             this.currentFloor = currentFloor;
         }
+
+        public int getId() {
+            return this.id;
+        }
     }
 
     private ArrayList<ElevatorReference> elevatorRefs;
@@ -36,8 +45,22 @@ public class Controller {
         this.elevatorRefs = new ArrayList<ElevatorReference>();
     }
 
-    public void requestElevator(int floor) {
+    public int requestElevator(int floor) {
+        // See if any elevators are currently waiting at the given floor
+        ListIterator<ElevatorReference> iterator =  this.elevatorRefs.listIterator();
+        while (iterator.hasNext()) {
+            ElevatorReference ref = iterator.next();
+            if (ref.currentFloor == floor && ref.direction == Direction.NotMoving) {
+                // send elevator
 
+                return ref.getId();
+            }
+        }
+//        for (int i = 0; i < this.elevatorRefs.size(); i++) {
+//            if (this.elevatorRefs.)
+//        }
+
+        return -1;
     }
 
     public void updateElevatorStatus(int elevatorId, int currentFloor, boolean doorsOpen) {
@@ -48,7 +71,10 @@ public class Controller {
 
     public int getElevatorId() {
         int newId = this.elevatorRefs.size();
-        this.elevatorRefs.add(new ElevatorReference());
+        this.elevatorRefs.add(new ElevatorReference(newId));
         return newId;
     }
+
+
+
 }
--- simulation/src/Simulation.java
+++ simulation/src/Simulation.java
@@ -1,18 +1,18 @@
 public class Simulation {
     public static void main(String[] args) {
+        // Maybe set these from command line args...
         final int elevatorCount = 5;
         final int floorCount = 8;
 
-        // Initialize the controller
+        // Create the controller
         Controller controller = new Controller(floorCount);
 
-        // Initialize the elevators
+        // Create the elevators
         Elevator[] elevators = new Elevator[elevatorCount];
         for (int i = 0; i < elevatorCount; i++) {
             elevators[i] = new Elevator(controller);
         }
 
         controller.requestElevator(6);
-
     }
 }
